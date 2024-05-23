c9f4723d75cc4c2f1450e8a59dc29d5c742b9917
Mishra-Nitya <mishranitya.0912@gmail.com>
2024-05-23 18:41:43 +0530

    Update lists(5).py

--- lists(5).py
+++ lists(5).py
@@ -1,4 +1,4 @@
-"""
+
 l = [2200, 2350, 2600, 2130, 2190]
 print(l[0] - l[1]) if l[0]>l[1] else print(l[1] - l[0])
 print(l[0] + l[1] + l[2])
@@ -7,7 +7,7 @@ l.append(1980)
 print(l)
 l[3] = l[3] - 200
 print(l)
-"""
+
 heros = ['spider man', 'thor', 'hulk', 'iron man', 'captain america']
 
 print(len(heros))
