7193c207478eea45f665df098356adf0c749c8c4
Mishra-Nitya <mishranitya.0912@gmail.com>
2024-05-24 20:00:14 +0530

    exc done

--- function.py
+++ function.py
@@ -1,1 +1,17 @@
 
+def calculate_area(b, h):
+    return (1/2)*b*h
+print(calculate_area(2,8))
+
+def calculate_area(b, h, s):
+    if s == 'triangle':
+        return (1/2)*b*h
+    elif s == 'rectangle':
+        return b*h
+print(calculate_area(2,8, 'triangle'))
+print(calculate_area(2,8, 'rectangle'))
+
+def print_pattern(n):
+    for i in range(n):
+        print("*"*(i+1))
+print_pattern(int(input()))
