79e624aa62e8f213f7c602ebb919ca0cea254513
Mishra-Nitya <mishranitya.0912@gmail.com>
2024-05-28 20:17:44 +0530

    exc done

--- inheritance.py
+++ inheritance.py
@@ -1,1 +1,12 @@
-
+class Animal:
+    def __init__(self) -> None:
+        pass
+    def habitat(self):
+        print("this is the method for animal class")
+class Dog(Animal):
+    def __init__(self) -> None:
+        super().__init__()
+    def habitat(self):
+        print("this is the method for dog class")
+d = Dog()
+d.habitat()
