9975fb7ac38eaa8fd309dc76cf07efc83d1de14c
Mishra-Nitya <mishranitya.0912@gmail.com>
2024-05-28 20:40:35 +0530

    exc done

--- multiple_inheritance.py
+++ multiple_inheritance.py
@@ -1,1 +1,15 @@
-
+class Teacher:
+    def Teacher(self):
+        print("this person is a teacher")
+class Student:
+    def Student(Self):
+        print("this persn is a student")
+class YouTuber:
+    def YouTuber(self):
+        print("this person is a YouTuber")
+class Person(Teacher, Student, YouTuber):
+    pass
+a = Person()
+a.Student()
+a.Teacher()
+a.YouTuber()
