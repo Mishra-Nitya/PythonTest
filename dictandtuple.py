ebcec655076e056a6fc79b6c197e4c3f178f17e9
Mishra-Nitya <mishranitya.0912@gmail.com>
2024-05-28 19:53:29 +0530

    exc done

--- dictandtuple.py
+++ dictandtuple.py
@@ -1,4 +1,4 @@
-'''
+
 dic = {'china' : 143, 'india' : 136, 'USA' : 32, 'pakistan' : 21}
 a = input("enter the country to add")
 if a in dic:
@@ -12,8 +12,26 @@ dic.pop(c)
 print(dic)
 d = input("enter country to enquire")
 print(dic[d])
-'''
+
+def avg(i):
+    sum = 0
+    for n in i:
+        sum += n
+    return sum/len(i)
 dicti = {'info' : [600, 630, 620], 'ril' : [1430, 1490, 1567], 'mtl' : [234,180,160]}
 op = input("enter your choice")
 if op.lower() == 'print':
-    print("info ==> ", dicti['info'], "==> avg: ")
+    print("info ==> ", dicti['info'], "==> avg: ", avg(dicti['info']), "\n", "ril ==> ", dicti['ril'], "==> avg: ", avg(dicti['ril']), "\n", "mtl ==> ", dicti['mtl'], "==> avg: ", avg(dicti['mtl']))
+elif op.lower() == 'add':
+    n = input("enter the stock ticker")
+    a = eval(input("enter the price"))
+    dicti[n] = a
+    print(dicti)
+
+def circle_calc(r):
+    ar = 3.14*(r**2)
+    cr = 2*3.14*r
+    d = r*2
+    return ar, cr, d
+
+print(circle_calc(10))
