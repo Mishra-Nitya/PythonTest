8997d17a45cb8f38134dcd2e8689b590f5d5515c
Mishra-Nitya <mishranitya.0912@gmail.com>
2024-05-28 20:07:07 +0530

    exc done

--- class_and_object.py
+++ class_and_object.py
@@ -1,1 +1,12 @@
+class Employee:
+    def __init__(self, id, name) -> None:
+        self.id = id
+        self.name = name
+emp = Employee(1, "coder")
+n = Employee(2, "nitya")
+print(emp.name)
+del emp.id
+print(n.id)
+#print(emp.id)
+del emp
 
