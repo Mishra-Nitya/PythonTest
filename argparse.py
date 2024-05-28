18cd310090b5f41dbfdbad375a662f3ac415555a
Mishra-Nitya <mishranitya.0912@gmail.com>
2024-05-29 00:08:17 +0530

    exc done

--- argparse.py
+++ argparse.py
@@ -1,1 +1,25 @@
+class AdultException(Exception):
+    pass
+
+class Person:
+
+    def __init__(self, name, age) -> None:
+        self.name = name 
+        self.age = age
+    
+    def get_minor_age(self):        
+        if self.age<18:
+            print("they are a minor")
+        else:
+            raise AdultException
+    def display(self):
+        try:
+            print(f'age -> {self.get_minor_age()}')
+        except AdultException:
+            print("they are an adult")
+        finally:
+            print(self.name, self.age)
+
+a = Person("nitya", 12)
+a.display()
 
