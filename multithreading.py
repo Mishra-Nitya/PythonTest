1779a64763c4e47c3afff72f24f299b00cfd0593
Mishra-Nitya <mishranitya.0912@gmail.com>
2024-05-29 19:24:35 +0530

    exc done

--- multithreading.py
+++ multithreading.py
@@ -1,1 +1,13 @@
+import threading
+import time
+from threading import Thread
 
+def sleep(i):
+    print("thread %i will sleep"% i)
+    time.sleep(5)
+    print("thread is awake")
+
+for i in range(10):
+    th = Thread(target = sleep, args = (i, ))
+    th.start()
+    print("curr threads = %i "% threading.active_count())
