6bcb584f591492b69a87ef5904e94a877c74890e
Mishra-Nitya <mishranitya.0912@gmail.com>
2024-05-29 19:16:47 +0530

    exc done

--- decorator.py
+++ decorator.py
@@ -1,1 +1,19 @@
 
+def checker(f):
+    def helper(x):
+        if type(x) == int and x > 0:
+            return f(x)
+        else:
+            raise Exception("arguement is not a non negtive integer")
+    return helper
+
+@checker
+def fact(n):
+    if n == 1:
+        return 1
+    return n*fact(n-1)
+
+try:
+    print(fact(1.0256))
+except Exception as e:
+    print("error")
